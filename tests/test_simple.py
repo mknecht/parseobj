0ad363ef55bd22a18d0e92e4eff8692b82bfa673
Murat Knecht <murat.knecht@googlemail.com>
2014-01-12 13:14:43 +0100

    Added foreach-test for multiple elements in collection

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,6 +1,17 @@
 from collections import defaultdict
+from itertools import izip
 
 from parseobj import foreach, ObjSyntaxError
+
+
+def assert_calls(recorder, func, *list_of_args):
+    assert len(recorder[func]) == len(list_of_args)
+    for actual, expected in izip(recorder[func], list_of_args):
+        _assertEqual(actual, expected)
+
+
+def _assertEqual(left, right):
+    assert left == right
 
 
 class TestForeach(object):
@@ -8,11 +19,11 @@ class TestForeach(object):
         called_with = defaultdict(lambda: [])
 
         def visit_element(*args):
-            called_with[visit_element].append(args)
+            called_with[visit_element].append(list(args))
 
         @foreach(visit_element)
-        def visit_collection(args):
-            called_with[visit_collection].append(args)
+        def visit_collection(*args):
+            called_with[visit_collection].append(list(args))
 
         col = []
         try:
@@ -21,28 +32,35 @@ class TestForeach(object):
         except ObjSyntaxError:
             # We're evaluating lazily, hence the collection will be visited,
             # before the missing elements are noticed.
-            self.assert_calls(called_with, visit_collection, [col])
-            self.assert_calls(called_with, visit_element)
+            assert_calls(called_with, visit_collection, [col])
+            assert_calls(called_with, visit_element)
 
     def test_given_one_element_expecting_functions_called(self):
         called_with = defaultdict(lambda: [])
 
-        def visit_element(element):
-            called_with[visit_element].append(element)
+        def visit_element(*args):
+            called_with[visit_element].append(list(args))
 
         @foreach(visit_element)
-        def visit_collection(collection):
-            called_with[visit_collection].append(collection)
+        def visit_collection(*args):
+            called_with[visit_collection].append(list(args))
 
         col = [1]
         visit_collection(col)
-        self.assert_calls(called_with, visit_collection, [col])
-        self.assert_calls(called_with, visit_element, [1])
+        assert_calls(called_with, visit_collection, [col])
+        assert_calls(called_with, visit_element, [1])
 
-    def assert_calls(self, recorder, func, *list_of_args):
-        assert len(recorder[func]) == len(list_of_args)
-        for args in list_of_args:
-            self._assertEqual(recorder[func], args)
+    def test_given_multiple_elements_expecting_functions_called(self):
+        called_with = defaultdict(lambda: [])
 
-    def _assertEqual(self, left, right):
-        assert left == right
+        def visit_element(*args):
+            called_with[visit_element].append(list(args))
+
+        @foreach(visit_element)
+        def visit_collection(*args):
+            called_with[visit_collection].append(list(args))
+
+        col = [1, 2, 3, 4]
+        visit_collection(col)
+        assert_calls(called_with, visit_collection, [col])
+        assert_calls(called_with, visit_element, [1], [2], [3], [4])
